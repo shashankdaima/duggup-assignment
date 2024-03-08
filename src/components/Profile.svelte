924af7160c29ffea54911642b60240b3f06b786c
Shashank Daima <daimashashank10@gmail.com>
2024-03-08 14:55:12 +0530

    init

---
+++ src/components/Profile.svelte
@@ -0,0 +1,33 @@
+<button class="transparent hover:bg-gray-100 rounded-md">
+
+	<div class="flex flex-row mr-4 ml-1 p-2">
+		<div class="circular-image my-auto">
+			<!-- image goes here -->
+			<img src="assets/profile_photo.png" width="42px" height="42px" alt="D" class="rounded-full" />
+		</div>
+
+		<div class="text my-auto mx-2">
+			<p class="text-lg text-gray-600">Shashank Daima</p>
+		</div>
+
+		<div class="arrow-down my-auto">
+			<div class="arrow-down">
+				<svg
+					width="12"
+					height="10"
+					viewBox="0 0 10 6"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M1 1L5 5L9 1"
+						stroke="#4B5563"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</div>
+		</div>
+	</div>
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Header from "../components/Header.svelte";
+
+</script>
+<main class="flex flex-col">
+    <Header/>
+    <div class="container mx-auto">
+        <h1 class="text-green-700">Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</main>
